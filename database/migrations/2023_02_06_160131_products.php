315f3a3cc5b4a308870e4c0e246b8985621d0762
Yaroslav <kdevyaroslav@gmail.com>
2023-02-06 18:16:12 +0200

    migration

---
+++ database/migrations/2023_02_06_160131_products.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_name');
+            $table->bigInteger('seller_id')->unsigned()->index()->nullable();
+            
+            $table->string('display_size');
+            $table->integer('quantity');
+            $table->integer('cost');
+            $table->timestamps();
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
